af3bef24dadb0b489659b2b43dca3a943dc22069
HoangTam1 <tamhv@fimo.edu.vn>
2019-04-18 19:31:00 +0700

    aaaa

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Hello, world")
+var http = require('http');
+var url = require('url');
+const host = process.env.HOST;
+const port = process.env.PORT;
+http.createServer(function (req, res) {
+if (req.url=="/hello"){
+  if(req.method=="GET")
+    res.end('world');
+
+  else if (req.method=="POST")
+    res.end('world created');
+
+  else if (req.method=="PUT")
+    res.end('world updated');
+
+  else (req.method=="DELETE")
+    res.end('world deleted');
+}
+}).listen(8000);
